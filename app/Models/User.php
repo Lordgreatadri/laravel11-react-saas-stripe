ba3c836dff511689dcd1f9955dc9576f03d52743
Lordgreat <rexmerlo@gmail.com>
2024-09-02 11:22:39 +0000

    seeders added for testing

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Attributes\ObserverdBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
+
+#[ObserverdBy(UserObserver::class)]
 
 class User extends Authenticatable
 {
@@ -20,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'available_credits'
     ];
 
     /**
